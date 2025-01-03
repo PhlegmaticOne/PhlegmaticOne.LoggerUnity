71afb7d936535c13070d9e30fbaf314f1894859b
Aleksandr Krotov <a.krotov@openmygame.com>
2025-01-03 16:56:06 +0300

    Methods for creating LogMessage

--- Runtime/Messages/LogMessage.cs
+++ Runtime/Messages/LogMessage.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Threading;
+using Openmygame.Logger.Configuration;
 
 namespace Openmygame.Logger.Messages
 {
     public struct LogMessage
     {
         private static int CurrentId = -1;
+
+        public static LogMessage Plain(LogLevel logLevel, Exception exception = null)
+        {
+            return new LogMessage(logLevel, LoggerConfigurationData.MessageFormat, exception);
+        }
+
+        public static LogMessage FromException(Exception exception)
+        {
+            return new LogMessage(LogLevel.Fatal, LoggerConfigurationData.ExceptionPlaceholderFormat, exception);
+        }
         
         public LogMessage(LogLevel logLevel, string format, Exception exception = null)
         {
